c654c19dc1a62a22456b4278fe57ce65f9855f8c
phinnik <phinnik@mail.ru>
2018-09-24 22:35:54 +0300

    completed RSA

--- hw1/rsa.py
+++ hw1/rsa.py
@@ -1,4 +1,5 @@
 from math import sqrt 
+
 def is_prime(n):
     """
     >>> is_prime(2)
@@ -26,8 +27,40 @@ def gcd(a, b):
     while a!=0 and b!=0:
         if a > b:
             a = a % b
-        else b = b % s
+        else:
+            b = b % a
     return (a + b)
+
+def multiplicative_inverse(e, phi):
+    """
+    >>> multiplicative_inverse(7, 40)
+    23
+    """
+    # PUT YOUR CODE HERE
+    
+    table = [[phi],         # A
+            [e],            # B
+            [phi % e],      # A%B
+            [phi // e],     #A//B
+            [],             # x
+            []]             # y
+    
+    while table[2][-1] != 0:
+        table[0].append(table[1][-1])
+        table[1].append(table[2][-1])
+        table[2].append(table[0][-1] % table[1][-1])
+        table[3].append(table[0][-1] // table[1][-1])
+
+    table[4] = [0 for i in range(len(table[0]))]
+    table[5] = [0 for i in range(len(table[0]))]
+    table[4][-1], table[5][-1] = 0, 1
+
+    for i in range(len(table[0])-2, -1, -1):
+        table[4][i] = table[5][i+1]
+        table[5][i] = table[4][i+1] - table[5][i+1]*table[3][i]
+
+    d = table[5][0] % table[0][0]
+    return d
 
 def generate_keypair(p, q):
     if not (is_prime(p) and is_prime(q)):
@@ -55,3 +88,5 @@ def generate_keypair(p, q):
     # Return public and private keypair
     # Public key is (e, n) and private key is (d, n)
     return ((e, n), (d, n))
+
+print(gcd(40,566))
