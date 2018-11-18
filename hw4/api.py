6ecda822bb5cd57c0c8e83e6ab81de9c254b4437
phinnik <phinnik@mail.ru>
2018-11-18 17:41:44 +0300

    adding TODOs and going home

--- hw4/api.py
+++ hw4/api.py
@@ -1,25 +1,22 @@
+# TODO: separate graphing stuff and vk analisys with different files
+# TODO: write excecute function to get data from vk faster
+# TODO: remake graphing function. Add more settings
+
+
 import requests
-#from datetime import datetime
 import time
-#import plotly
-#import plotly.plotly as py
-#import plotly.graph_objs as go
-import config
+from datetime import datetime
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
 import igraph
 from igraph import Graph, plot
 import numpy as np
-
-#plotly.tools.set_credentials_file(username=config.PLOTLY['USERNAME'], api_key=config.PLOTLY['API_KEY'])
+import config
 
 
 def get(url, params={}, timeout=5, max_retries=5, backoff_factor=0.3):
-    """ Выполнить GET-запрос
-    :param url: адрес, на который необходимо выполнить запрос
-    :param params: параметры запроса
-    :param timeout: максимальное время ожидания ответа от сервера
-    :param max_retries: максимальное число повторных запросов
-    :param backoff_factor: коэффициент экспоненциального нарастания задержки
-    """
+    """ Выполнить GET-запрос c экспоненциальным нарастанием задержки при ошибке """
     request_status = False
     retries = 1
     while (request_status == False) and (retries < max_retries):
@@ -29,26 +26,24 @@ def get(url, params={}, timeout=5, max_retries=5, backoff_factor=0.3):
         except requests.RequestException as e:
             time.sleep(timeout * backoff_factor * retries)
             retries += 1
-    #print(r.json())
     return r.json()['response']
 
 
 def is_api_ok():
+    # функция для отладки. Проверка, доступно ли api
     url = "https://api.vk.com/method/friends.get"
     parameters = {'access_token': config.VK['VK_ACCESS_TOKEN'],
                   'user_id': config.VK['MY_ID'],
                   'fields': '',
                   'count': 1,
                   'v': config.VK['API_VERSION']}
-    r = requests.get(url, params=parameters)
-    return r.json()
+    r = requests.get(url, params=parameters).json()
+    print(r)
+    return r
 
 
 def get_friends(user_id, fields='', count=5):
-    """ Вернуть данных о друзьях пользователя
-    :param user_id: идентификатор пользователя, список друзей которого нужно получить
-    :param fields: список полей, которые нужно получить для каждого пользователя
-    """
+    """ Вернуть данныe о друзьях пользователя """
     assert isinstance(user_id, int), "user_id must be positive integer"
     assert isinstance(fields, str), "fields must be string"
     assert user_id > 0, "user_id must be positive integer"
@@ -65,7 +60,7 @@ def get_friends(user_id, fields='', count=5):
 
 
 def select_friends(friends):
-    """ Выборка друзей с полной датой рождения"""
+    """ Выборка друзей с полной датой рождения """
     good_friends = []
     for fr in friends:
         if 'bdate' in fr:
@@ -76,9 +71,7 @@ def select_friends(friends):
 
 def age_predict(user_id):
     """ Наивный прогноз возраста по возрасту друзей
-    Возраст считается как медиана среди возраста всех друзей пользователя
-    :param user_id: идентификатор пользователя
-    """
+    Возраст считается как медиана среди возраста всех друзей пользователя """
     assert isinstance(user_id, int), "user_id must be positive integer"
     assert user_id > 0, "user_id must be positive integer"
 
@@ -93,11 +86,7 @@ def age_predict(user_id):
 
 
 def messages_get_history(user_id, offset=0, count=200):
-    """ Получить историю переписки с указанным пользователем
-    :param user_id: идентификатор пользователя, с которым нужно получить историю переписки
-    :param offset: смещение в истории переписки
-    :param count: число сообщений, которое нужно получить
-    """
+    """ Получить историю переписки с указанным пользователем """
     assert isinstance(user_id, int), "user_id must be positive integer"
     assert user_id > 0, "user_id must be positive integer"
     assert isinstance(offset, int), "offset must be positive integer"
@@ -118,7 +107,6 @@ def messages_get_history(user_id, offset=0, count=200):
 
 def get_many_messages(user_id, count, offset=0):
     """ возвращает список сообщений учитывая ограничения api"""
-    
     messages = []
     start_time = time.time()
     for i in range(0, count, 200):
@@ -131,9 +119,7 @@ def get_many_messages(user_id, count, offset=0):
 
 
 def count_dates_from_messages(messages):
-    """ Получить список дат и их частот
-    :param messages: список сообщений
-    """
+    """ Получить список дат и их частот """
     freq_list = [[], []]
     for mes in messages:
         date = datetime.fromtimestamp(mes['date']).strftime("%Y-%m-%d")
@@ -147,9 +133,7 @@ def count_dates_from_messages(messages):
 
 
 def plotly_messages_freq(freq_list):
-    """ Построение графика с помощью Plot.ly
-    :param freq_list: список дат и их частот
-    """
+    """ Построение графика с помощью Plot.ly """
     x = freq_list[0]
     y = freq_list[1]
     data = [go.Scatter(x=x, y=y)]
@@ -157,9 +141,13 @@ def plotly_messages_freq(freq_list):
 
 
 def get_network(users_ids, as_edgelist=True):
+
     network = []
-    for user in users_ids:
-        time.sleep(3/2.5)
+    start_time = time.time()
+    for i, user in enumerate(users_ids):
+        # sleep for not to be banned
+        if i % 3 == 0:
+            time.sleep(time.time() - start_time)
         # use try to exclude blocked users
         try:
             user_friends = set(get_friends(user, count=0)['items'])
@@ -172,25 +160,13 @@ def get_network(users_ids, as_edgelist=True):
     return network
 
 
-"""my_friends = get_friends(config.VK['MY_ID'], count=0)['items']
-ntw = get_network(my_friends)
-
-f = open('ntw.py', 'w')
-f.write('ntw = {}   '.format(ntw))
-f.close()"""
-
-
-
-from ntw import ntw
-
 def plot_graph(network):
-    # Создание вершин и ребер
-    edges = network
-    vertices = [i for i in range(len(edges))]
+    # Создание вершин
+    vertices = [i for i in range(len(network))]
 
     # Создание графа
     g = Graph(vertex_attrs={"label":vertices},
-        edges=edges, directed=False)
+        edges=network, directed=False)
 
     # Задаем стиль отображения графа
     N = len(vertices)
@@ -212,4 +188,10 @@ def plot_graph(network):
     # Отрисовываем граф
     plot(g, **visual_style)
 
-plot_graph(ntw)
+
+def main():
+    # login to your plotly account
+    plotly.tools.set_credentials_file(username=config.PLOTLY['USERNAME'], api_key=config.PLOTLY['API_KEY'])
+
+if __name__ == '__main__':
+    main()
